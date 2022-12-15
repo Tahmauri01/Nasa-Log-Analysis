9d81f5e089df716bf7d375a95e24c2b8732bc07d
Tahmauri01 <tbobo@brooklynsteamcenter.org>
2022-12-15 10:53:00 -0500

    made the most frequent stuff!

--- Main.py
+++ Main.py
@@ -2,11 +2,21 @@ from collections import Counter
 myFile = open("NASA_access_log_Jul95")
 
 ip_addresses = []
+dates = []
+directories = []
+
 
 try:
     for lines in myFile:
         x = lines.split(" ")[0]
+        y = lines.split(" ")[3]
+        y = y.split(":")[0]
+        z = lines.split('"')[1]
+        z = z.split('.')[1]
+        z = z.split(" ")[0]
         ip_addresses.append(x)
+        dates.append(y)
+        directories.append(z)
 
         
         
@@ -16,5 +26,9 @@ try:
 except:
     print("uh oh")
 
-print(Counter(ip_addresses))
+
+
+print("The most frequent IP address was " + str(Counter(ip_addresses).most_common(1)))
+print("The most frequent date was " + str(Counter(dates).most_common(1))) 
+print("The most frequent file type was " + str(Counter(directories).most_common(7)))
 
