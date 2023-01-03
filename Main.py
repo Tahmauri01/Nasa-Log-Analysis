623a5744cd31b83318726963e49e0cbe9b837066
Tahmauri01 <tbobo@brooklynsteamcenter.org>
2023-01-03 10:51:30 -0500

    Readme file created

--- Main.py
+++ Main.py
@@ -14,8 +14,9 @@ try:
         y = lines.split(" ")[3]
         y = y.split(":")[0]
         z = lines.split('"')[1]
-        z = z.split('.')[1]
-        z = z.split(" ")[0]
+        if '.' in z:
+            z = z.split('.')[1]
+            z = z.split(" ")[0]
         ip_addresses.append(x)
         dates.append(y)
         directories.append(z)
@@ -25,25 +26,83 @@ try:
         #print(lines.split(" ")[0])
         
         #print("uh-oh")
-except:
+except UnicodeDecodeError:
+    print(lines)
     print("uh oh")
-
+print(lines)
 ip_counter = Counter(ip_addresses)
 #Counter(ip_counter.most_common(3))
 
 
 
 print("The most frequent IP address was " + str(Counter(ip_addresses).most_common(3)))
-print("The most frequent date was " + str(Counter(dates).most_common(1))) 
+print("The most frequent date was " + str(Counter(dates).most_common(4))) 
 print("The most frequent file type was " + str(Counter(directories).most_common(7)))
 
+fig, ax = plt.subplots()
+
+# print(ip_counter.keys())
+# print(ip_counter.values())
+
+keyIP = []
+valueIP = []
+
+ip_counter = Counter(ip_addresses).most_common(20)
+
+for ip in ip_counter:
+   keyIP.append(ip[0])
+   valueIP.append(ip[1])
+
+plt.bar(keyIP, valueIP)
+
+plt.xticks(rotation=30)
+
+ax.set_title('Most Common IP Addresses')
+ax.set_xlabel('Ip Addresses')
+
+plt.show()
 
 
-print(ip_counter.keys())
-print(ip_counter.values())
 
-plt.figure(figsize=((10), (7)))
 
-plt.bar(ip_counter.keys(), ip_counter.values(), align='edge', width=30)
+keyTime = []
+valueTime = []
+
+Time_counter = Counter(dates).most_common()
+
+for time in Time_counter:
+   keyTime.append(time[0])
+   valueTime.append(time[1])
+
+plt.plot(keyTime, valueTime, marker='.', linestyle='-')
+
+plt.xticks(rotation=30)
+
+ax.set_title('Most Common Dates')
+ax.set_xlabel('Dates')
+
+plt.show()
+
+
+
+
+
+
+keyFile = []
+valueFile = []
+
+file_counter = Counter(directories).most_common(20)
+
+for files in file_counter:
+    if files[0] != '0':
+        keyFile.append(files[0])
+        valueFile.append(files[1])
+
+plt.bar(keyFile, valueFile)
+
+plt.xticks(rotation=30)
+
+ax.set_title('Most Common File Types')
+ax.set_xlabel('File Types')
 
 plt.show()
